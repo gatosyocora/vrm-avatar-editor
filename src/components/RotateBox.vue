f8924a1ac8e330ef38e373a6798951209c4323f6
gatosyocora <gatosyocora36@gmail.com>
2020-12-30 01:30:37 +0900

    テストとして回転するCubeを追加

---
+++ src/components/RotateBox.vue
@@ -0,0 +1,45 @@
+<!-- https://qiita.com/kokorau/items/1c8ba182699b3646b363 -->
+<template>
+  <canvas id="canvas" width="600" height="400"></canvas>
+</template>
+
+<script type="ts">
+  import { Component, Vue } from "vue-property-decorator";
+  import * as THREE from "three";
+
+  @Component
+  export default class RotateBox extends Vue {
+    data() {
+      const scene = new THREE.Scene();
+      const renderer = null;
+      const camera = new THREE.PerspectiveCamera(75, 600/400, 0.1, 1000);
+      const light = new THREE.DirectionalLight(0xffffff);
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshNormalMaterial();
+      const cube = new THREE.Mesh(geometry, material);
+      return { scene, renderer, camera, light, geometry, material, cube };
+    }
+    mounted() {
+      const $canvas = document.getElementById("canvas");
+      this.renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        canvas: $canvas
+      });
+
+      this.camera.position.set(0, 0, 2);
+      this.light.position.set(0, 0, 10);
+      this.scene.add(this.cube);
+      this.scene.add(this.light);
+
+      this.animate();
+    }
+    animate() {
+        requestAnimationFrame(this.animate);
+
+        this.cube.rotation.x += 0.02;
+        this.cube.rotation.y += 0.02;
+
+        this.renderer.render(this.scene, this.camera);
+      }
+  };
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <rotate-box></rotate-box>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import RotateBox from '@/components/RotateBox.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    RotateBox,
   },
 })
 export default class Home extends Vue {}
