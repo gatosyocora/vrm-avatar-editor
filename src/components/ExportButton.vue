da6d97cd069bbb104ea0c3b925b8ab7a20308f13
gatosyocora <gatosyocora36@gmail.com>
2021-02-12 01:52:10 +0900

    GLTFLoaderを使用する用のコードを削除

--- src/components/ExportButton.vue
+++ src/components/ExportButton.vue
@@ -21,50 +21,6 @@
     public vrm: VRM | null  = null;
 
     public exportVRM() {
-        /*
-        const exporter = new GLTFExporter();
-
-        const options = {
-            trs: true,
-            //onlyVisible: true,
-            //truncateDrawRange: true,
-            //binary: true,
-            //embedImages: true,
-            //maxTextureSize: Infinity,
-            //animations: ,
-            //forceIndices: true,
-            //includeCustomExtensions: true,
-        };
-
-        const objects = [
-            //this.vrm.blendShapeProxy,
-            //this.vrm.firstPerson,
-            //this.vrm.humanoid,
-            //this.vrm.lookAt,
-            //this.vrm.materials,
-            //this.vrm.meta,
-            //this.vrm.scene,
-            //this.vrm.springBoneManager
-        ];
-
-        // JSONデータの下に画像ファイルデータをおいている
-        // gltfでJSON内のimages(画像を置くところ)にはメタデータだけおいている
-        // extensionsというものを追加して、VRMで必要なもの（meta, firstPersonなど）を追加している
-
-        exporter.parse(this.vrm.scene, (gltf) => {
-            console.log(gltf);
-
-            const fileName = "test.vrm";
-            const dataText = JSON.stringify(gltf, undefined, 2);
-            const blob = new Blob([dataText], {type:'text/plain'}, fileName);
-            const link = document.createElement("a");
-            link.href = URL.createObjectURL(blob);
-            link.download = fileName;
-            link.click();
-
-        }, options);
-        */
-
        if (!this.vrm) return;
 
        const exporter = new VRMExporter();
