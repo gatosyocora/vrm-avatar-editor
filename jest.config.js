b7d2e939a0bf28b064364f5c1b2760632d09b710
gatosyocora <gatosyocora36@gmail.com>
2021-02-15 19:43:14 +0900

    Cの大文字表記が非推奨になったのでtsconfigに変更

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     globals: {
       "ts-jest": {
-        "tsConfig": "tsconfig.json"
+        "tsconfig": "tsconfig.json"
       }
     },
     testURL: "http://localhost",
