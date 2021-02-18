cdd7fb6843db9e712bdbf4c39912adb9c3c24563
Koyashiro Kohaku <develop@koyashiro.net>
2021-02-19 00:02:45 +0900

    Jestのテストが通るように修正

--- jest.config.js
+++ jest.config.js
@@ -11,9 +11,10 @@ module.exports = {
     },
     transform: {
       ".*\\.(vue)$": "vue-jest",
-      "^.+\\.ts$": "ts-jest",
+      "^.+\\.ts$": "babel-jest",
       "^.+\\.js$": "babel-jest"
     },
+    transformIgnorePatterns: ['node_modules/(?!three).+\\.js$'],
     globals: {
       "ts-jest": {
         "tsconfig": "tsconfig.json"
