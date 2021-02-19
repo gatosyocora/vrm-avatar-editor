16293b21cc7be4f30b25715894c65d4a00828639
gatosyocora <gatosyocora36@gmail.com>
2021-02-19 18:13:04 +0900

    テストコードにコードフォーマッティングを実施

--- tests/Helloworld.test.ts
+++ tests/Helloworld.test.ts
@@ -1,9 +1,9 @@
 //import { shallowMount } from '@vue/test-utils'
 //import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-    test('test script', () => {
-        const msg = "Hello world"
-        expect(msg).toMatch(msg)
-    })
-})
+describe("HelloWorld.vue", () => {
+  test("test script", () => {
+    const msg = "Hello world";
+    expect(msg).toMatch(msg);
+  });
+});
